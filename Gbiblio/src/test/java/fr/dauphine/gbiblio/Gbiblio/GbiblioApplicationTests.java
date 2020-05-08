c48bc8afd3af0fa6b243d2eb1f34a93a460c4ba6
Unknown <iris.joliman@dauphine.eu>
2020-05-08 14:55:03 +0200

    Add Livre microservice

---
+++ Gbiblio/src/test/java/fr/dauphine/gbiblio/Gbiblio/GbiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dauphine.gbiblio.Gbiblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbiblioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
